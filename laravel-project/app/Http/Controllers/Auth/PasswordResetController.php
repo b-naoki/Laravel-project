766ce7d6dbb7b6c9ea0110efec6fdaef7e4e2f7d
matsumoto.n <matsumoto.n@b-style.net>
2024-09-22 04:24:56 +0900

    不要なコメントアウトを削除しました。

--- laravel-project/app/Http/Controllers/Auth/PasswordResetController.php
+++ laravel-project/app/Http/Controllers/Auth/PasswordResetController.php
@@ -10,13 +10,10 @@ class PasswordResetController extends Controller
 {
     public function sendResetLink(Request $request)
     {
-        // リクエストに含まれるメールアドレスのバリデーション
         $request->validate(['email' => 'required|email']);
 
-        // パスワードリセットリンクを送信
         $status = Password::sendResetLink($request->only('email'));
 
-        // リセットリンクが正常に送信された場合の処理
         return $status === Password::RESET_LINK_SENT
                     ? back()->with(['status' => __($status)])
                     : back()->withErrors(['email' => __($status)]);

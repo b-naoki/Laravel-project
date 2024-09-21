f80a609587ac78002d18c8c663a21ef8ff98c889
matsumoto.n <matsumoto.n@b-style.net>
2024-09-21 22:23:37 +0900

    不要なREADME.mdを削除しました。

--- laravel-project/pdo_test.php
+++ laravel-project/pdo_test.php
@@ -1,29 +1,15 @@
 <?php
 
-// データベースの設定をここに記述します
-$host = 'db'; // ホスト名
-$db   = 'laravel'; // データベース名
-$user = 'root'; // ユーザー名
-$pass = 'password'; // パスワード
-$charset = 'utf8mb4'; // 文字セット
-
-// root@b6b24271fa93:/var/www# ls
-// README.md  docker  docker-compose.yml  laravel-project
-// root@b6b24271fa93:/var/www# cd laravel-project/
-// root@b6b24271fa93:/var/www/laravel-project# ls
-// README.md  bootstrap      config        pdo_test.php  resources  tests
-// app        composer.json  database      phpunit.xml   routes     vendor
-// artisan    composer.lock  package.json  public        storage    vite.config.js
+// 使用方法は以下の通りです
 // root@b6b24271fa93:/var/www/laravel-project# php pdo_test.php
 // 接続成功
-// root@b6b24271fa93:/var/www/laravel-project#
-// DB_CONNECTION=mysql
-// DB_HOST=db
-// # DB_HOST=localhost
-// DB_PORT=3306
-// DB_DATABASE=laravel
-// DB_USERNAME=root
-// DB_PASSWORD=password
+
+// データベースの設定をここに記述します
+$host = 'db';
+$db   = 'laravel';
+$user = 'root';
+$pass = 'password';
+$charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
